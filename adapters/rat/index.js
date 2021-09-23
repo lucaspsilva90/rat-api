06e8067e96a49216084ec25f7e2d363d913c7fa4
Lucas Pereira e Silva <lucas.p.silva90@hotmail.com>
2021-09-23 17:48:26 -0300

    chore: created user and rat adapters

---
+++ adapters/rat/index.js
@@ -0,0 +1,48 @@
+const ratWrapper = ({
+  config,
+}) => {
+  const get = async ({
+    payload,
+    header,
+    onSuccess,
+    onError,
+  }) => {
+
+  };
+
+  const create = async ({
+    payload,
+    header,
+    onSuccess,
+    onError,
+  }) => {
+
+  };
+
+  const update = async ({
+    payload,
+    header,
+    onSuccess,
+    onError,
+  }) => {
+
+  };
+
+  const destroy = async ({
+    payload,
+    header,
+    onSuccess,
+    onError,
+  }) => {
+
+  };
+
+  return {
+    get,
+    create,
+    update,
+    destroy,
+  };
+};
+
+module.exports = ratWrapper;
